aec4cbd942f509bf450113909b46bfc4f375b9c7
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-08-15 18:46:08 +0300

    sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Mr. Chocolate", 5, "M", "Brown","Yes",1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Tabitha", 6, "F", "Brown","Yes",1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Sergeant Brown", 4, "M", "Black","No",0);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Melissa", 13, "F", "Gray","Yes",1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Rowdy", 10, "M", "Brown","Yes",1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Grinch", 2, "M", "Black","No",0);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES("Wendy", 6, "F", "Black","No",1);
+INSERT INTO bears(name, age, sex, color,temperament,alive) VALUES(NULL, NULL, "M", "Brown",NULL,1);
